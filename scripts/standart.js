a8cfddd68a9f13abb4239e4263c162545d281782
Ping <dev.bastianabel@gmail.com>
2024-11-15 00:25:33 +0100

    simple basket design added, still need pos sticky

--- scripts/standart.js
+++ scripts/standart.js
@@ -2,3 +2,8 @@ function changeClass(element, classname) {
     elementRef = element;
     elementRef.classList.toggle(classname);
 }
+
+function changeClassById(idname, classname) {
+    elementRef = document.getElementById(idname);
+    elementRef.classList.toggle(classname);
+}
