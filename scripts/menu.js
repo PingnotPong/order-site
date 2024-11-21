edddbebff8836f7b306144afc72b7ae548558ebf
Ping <dev.bastianabel@gmail.com>
2024-11-21 15:17:41 +0100

    shorten functions and added new functions with the same content

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,6 +3,7 @@ const basketContRef = document.getElementById("basketContainer");
 const navRef = document.getElementById("nav");
 const menuRef = document.getElementById("menu");
 const buttonSumRef = document.getElementById("displaySum");
+const delcost = restaurants["bombay-mirchi"]["liefergebühr"];
 
 function getBasketElements() {
     basketRef = document.getElementById("basket");
@@ -33,83 +34,101 @@ function renderBasket() {
     if(basket.length > 0) {
         basketContRef.innerHTML += getBasketBaseTemplate();
         getBasketElements()
-        for (let i = 0; i < basket.length; i++) {
-            dName = basket[i]["name"]
-            dPrice = basket[i]["price"]
-            dCount = basket[i]["count"]
-            dI = basket[i]["index"]
-            dCat = basket[i]["category"]
-            basketRef.innerHTML += getBasketItemTemplate(dName, dPrice.toFixed(2) + "€", dCount, dCat, dI);
-        } 
-        sum = 0;
-        delcost = restaurants["bombay-mirchi"]["liefergebühr"];
-        for (let y = 0; y < basket.length; y++) {
-            sum = sum + (basket[y]["count"] * basket[y]["price"]);
-        }
-        subtotalRef.innerHTML = sum.toFixed(2) + "€";
-        deliveryCostsRef.innerHTML = delcost.toFixed(2) + "€";
-        totalSumRef.innerHTML = (sum + delcost).toFixed(2) + "€";
-        buttonSumRef.innerHTML = "(" + (sum + delcost).toFixed(2) + "€" + ")";
+        renderBasketItem()
+        calculateSum()
+        updateSums()
     } else {
         basketContRef.innerHTML = getBasketPlaceholderTemplate()
         buttonSumRef.innerHTML = "(0.00€)";
     }
 }
 
+function renderBasketItem() {
+    for (let i = 0; i < basket.length; i++) {
+        dName = basket[i]["name"]
+        dPrice = basket[i]["price"]
+        dCount = basket[i]["count"]
+        dI = basket[i]["index"]
+        dCat = basket[i]["category"]
+        basketRef.innerHTML += getBasketItemTemplate(dName, dPrice.toFixed(2) + "€", dCount, dCat, dI);
+    } 
+}
+
 function editBasket(action, cat, i) {
-    if (cat == undefined) {} else {
+    if (cat == undefined) {
+        basket = [];
+        window.location.href = "/pages/ordered.html";
+    } else {
         nameTest = restaurants["bombay-mirchi"]["kategorien"][cat];
-        nameTest = nameTest[i]["name"];        
-    }
-    switch (action) {
-        case "plus":
-            result = false;
-            for (let y = 0; y < basket.length; y++) {
-                if (basket[y]["name"] == nameTest) {
-                    result = true;
-                    basket[y]["count"]++;
-                    break;
-                } else {
-                    result = false
-                }
-            }
+        nameTest = nameTest[i]["name"];  
+        if (action == "plus") {
+            basketAddCount()
             if (result == false) {
-                basket.push({
-                    "name" : restaurants["bombay-mirchi"]["kategorien"][cat][i]["name"], 
-                    "price" : restaurants["bombay-mirchi"]["kategorien"][cat][i]["price"],
-                    "count" : 1,
-                    "index" : i,
-                    "category" : cat
-                    });
+                basketPushNewItem(cat, i)
             }
-            break;
-        case "minus":
-            for (let y = 0; y < basket.length; y++) {
-                if (nameTest == basket[y]["name"]) {
-                    if(basket[y]["count"] > 1) {
-                        basket[y]["count"]--;
-                        break;    
-                    } else {
-                        basket.splice(y, 1)
-                    }
-                } else{}
-            }
-            break;
-        case "delete":
-            for (let y = 0; y < basket.length; y++) {
-                if (nameTest == basket[y]["name"]) {
-                    basket.splice(y, 1)
-                } else{}             
-            }
-            break;
-        case "buy":
-            basket = [];
-            renderBasket();
-            // window.location.replace("/pages/ordered.html")
-            window.location.href = "/pages/ordered.html";
-            break;
-        default:
-            break;
+        }else if (action == "minus") {
+            basketMinusCount()
+        }else if (action == "delete") {
+            basketDeleteItem()
+        }
     }
     renderBasket();
+}
+
+function calculateSum() {
+    sum = 0;
+    for (let y = 0; y < basket.length; y++) {
+        sum = sum + (basket[y]["count"] * basket[y]["price"]);
+    }
+}
+
+function basketAddCount() {
+    result = false;
+    for (let y = 0; y < basket.length; y++) {
+        if (basket[y]["name"] == nameTest) {
+            result = true;
+            basket[y]["count"]++;
+            break;
+        } else {
+            result = false
+        }
+    }
+}
+
+function basketMinusCount() {
+    for (let y = 0; y < basket.length; y++) {
+        if (nameTest == basket[y]["name"]) {
+            if(basket[y]["count"] > 1) {
+                basket[y]["count"]--;
+                break;    
+            } else {
+                basket.splice(y, 1)
+            }
+        } else{}
+    }
+}
+
+function basketDeleteItem() {
+    for (let y = 0; y < basket.length; y++) {
+        if (nameTest == basket[y]["name"]) {
+            basket.splice(y, 1)
+        } else{}             
+    }
+}
+
+function basketPushNewItem(cat, i) {
+    basket.push({
+        "name" : restaurants["bombay-mirchi"]["kategorien"][cat][i]["name"], 
+        "price" : restaurants["bombay-mirchi"]["kategorien"][cat][i]["price"],
+        "count" : 1,
+        "index" : i,
+        "category" : cat
+        });
+}
+
+function updateSums() {
+    subtotalRef.innerHTML = sum.toFixed(2) + "€";
+    deliveryCostsRef.innerHTML = delcost.toFixed(2) + "€";
+    totalSumRef.innerHTML = (sum + delcost).toFixed(2) + "€";
+    buttonSumRef.innerHTML = "(" + (sum + delcost).toFixed(2) + "€" + ")";
 }
